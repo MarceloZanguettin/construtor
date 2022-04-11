cb689871ebd0a10dc3bd1acf638d8a072a028e1d
MarceloZanguettin <marcelo.pereira38@fatec.sp.gov.br>
2022-04-11 18:45:23 +0000

    Exemplo

---
+++ Carro.java
@@ -0,0 +1,29 @@
+public class Carro {
+    private String motor;
+    private String modelo;
+    private int rodas;
+    private double preco;
+    private String tracao;
+
+    public Carro(){
+
+    }
+    
+    public Carro(String motor, String modelo, int rodas, double preco){
+
+        this.motor=motor;
+        this.modelo=modelo;
+        this.rodas=rodas;
+        this.preco=preco;
+
+    }
+
+    public Carro(String motor, String modelo, int rodas, double preco, String tracao){
+
+    }
+    public void dadosVeiculo(){
+        System.out.println(this.motor);
+
+    }
+
+}
---
+++ Principal.java
@@ -0,0 +1,12 @@
+public class Principal {
+
+    public static void main(String[] args){
+        Bicicleta bike = new Bicicleta();
+
+        Carro car = new Carro();
+        Carro car2 = new Carro("1.0", "Up", 4, 63000);
+
+        car2.dadosVeiculo();
+    }
+    
+}
